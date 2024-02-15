1aabffefa657ad5cbcc27a41b3b8df09deba666e
Diba Khademi <dibakhademi@192.168.1.147>
2024-02-15 18:05:16 +0000

    Add graceful shutdown

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"testModule/handlers"
+	"time"
 )
 
 func main() {
@@ -20,6 +23,8 @@ func main() {
 		// fmt.Fprintf(rw, "Input %s", d)
 	// })
 
+	// http.ListenAndServe(":9090", serveMux)
+
 	// -------------- Refactored version using hello handler
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	helloHandler := handlers.NewHello(l)
@@ -30,5 +35,30 @@ func main() {
 	serveMux.Handle("/", helloHandler)
 	serveMux.Handle("/goodbye" , goodbyeHandler)
 
-	http.ListenAndServe(":9090", serveMux)
+	server := &http.Server{
+		Addr: ":9090",
+		Handler: serveMux,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	l.Println("Received Terminate, graceful shutdown", sig)
+
+	timeout := time.Now().Add(30 * time.Second)
+	timeoutContext, _ := context.WithDeadline(context.Background(), timeout)
+	server.Shutdown(timeoutContext) // when we call Shutdown server will not accept any more requests, and let the in progress requests finish then it will shut down the server
 }
+ 
